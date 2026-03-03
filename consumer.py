8c14259579900064df5495a086b1c30fd5a50a32
Stan Makov <makovstpm@gmail.com>
2026-03-03 23:24:25 +0100

    Add consumer.py

---
+++ consumer.py
@@ -0,0 +1,13 @@
+from kafka import KafkaConsumer
+import json
+
+consumer = KafkaConsumer(
+    "line_events",
+    bootstrap_servers='localhost:9092',
+    value_deserializer=lambda m: json.loads(m.decode('utf-8')),
+    auto_offset_reset="latest"
+)
+
+for message in consumer:
+    print(message.value)
+
